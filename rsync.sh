d85a0f38c7b1162e1cd7040b3be4d54fad68519c
Kazuhiro Misumi <misumi@criepi.denken.or.jp>
2017-02-12 01:31:42 +0900

    Added how to copy specific files with wildcard.

--- rsync.sh
+++ rsync.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+# http://tech.nitoyon.com/ja/blog/2013/03/26/rsync-include-exclude/ 
 
 logfile=rsync.`/bin/date +%y%m%d_%H%M%S`.log
 
 {
   echo "Script starting on "`date`
-  rsync -av /data36/misumi/archive/ /data38/misumi/ccsm
+  rsync -av --include="*sta*.nc" --exclude="*" /share/data38/gtucmt/2016/smD2Kfull/result/  tide0
+  rsync -av --include="*sta*.nc" --exclude="*" /share/data38/gtucmt/2016c/smD2Kfull/result/ tide1Hidaka
   echo "Script ending on "`date`
 } >> "$logfile" 2>&1
-
